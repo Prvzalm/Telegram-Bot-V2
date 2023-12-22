f711375758d8b09ffb7044acc337a8a2544db25f
Prvzalm <prvzalm69@gmail.com>
2023-12-22 15:48:14 +0530

    Inserted the count and database code

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-const TelegramBot = require("node-telegram-bot-api");
-const axios = require("axios");
-const express = require('express');
-require("dotenv").config();
+const TelegramBot = require('node-telegram-bot-api');
+const mongoose = require('mongoose');
+const express = require("express")
+require("dotenv").config()
 
 const app = express();
 
-const TOKEN = process.env.TOKEN
-const bot = new TelegramBot(TOKEN, { polling: true });
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Get!');
+// Replace with your Telegram bot token
+const botToken = process.env.TOKEN;
+const chatId = process.env.CHATID;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/telegram_tracking');
+
+// Create a schema for tracking information
+const trackingSchema = new mongoose.Schema({
+  inviteLink: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  joinedCount: {
+    type: Number,
+    default: 0,
+  },
+  leftCount: {
+    type: Number,
+    default: 0,
+  },
 });
 
-bot.on('message', (msg) => {
+// Create a model for tracking information
+const Tracking = mongoose.model('Tracking', trackingSchema);
 
-  var hi = "hi";
-  if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
-  bot.sendMessage(msg.chat.id,"Hello dear user");
+// Create a new Telegram bot
+const bot = new TelegramBot(botToken, { polling: true });
+
+// Event handler for updates in chat members
+bot.on('chatMemberUpdated', async (update) => {
+  console.log(update)
+  const chatId = update.chat.id;
+  const inviteLink = getInviteLinkFromChat(update.chat);
+  const status = update.new_chat_member.status; // 'member' or 'left' or 'kicked'
+
+  try {
+    let trackingInfo = await Tracking.findOne({ inviteLink });
+
+    if (!trackingInfo) {
+      trackingInfo = new Tracking({ inviteLink });
+    }
+
+    if (status === 'member') {
+      // User joined the chat
+      trackingInfo.joinedCount += 1;
+    } else if (status === 'left' || status === 'kicked') {
+      // User left or was kicked from the chat
+      trackingInfo.leftCount += 1;
+    }
+
+    await trackingInfo.save();
+  } catch (error) {
+    console.error('Error updating tracking information:', error.message);
   }
-  
-  var bye = "bye";
-  if (msg.text.toString().toLowerCase().includes(bye)) {
-  bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-  }
 });
 
-bot.onText(/\/start/, (msg) => {
+// Helper function to extract the invite link from the chat
+function getInviteLinkFromChat(chat) {
+  return chat.description || '';
+}
 
-  bot.sendMessage(msg.chat.id, "Welcome");
-  
-});
-
-bot.on('message', (msg) => {
-
-  var bye = "bye";
-  if (msg.text.toString().toLowerCase().includes(bye)) {
-  bot.sendMessage(msg.chat.id, "Have a nice day " + msg.from.first_name);
-  }
-  
-});
-
-app.listen(3000, () => {
-  console.log(`Server running at Port 3000`);
-});
